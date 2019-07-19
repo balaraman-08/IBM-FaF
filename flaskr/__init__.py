776660537105698eb257e0caa72ee355f2e8910e
BALARAMAN <balaramanmuthupandi@gmail.com>
2019-07-19 10:17:47 +0530

    Set session secret

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 def create_app():
     # create and configure the app
     app = Flask(__name__)
+    app.secret_key = 'willbereplacedbyarandomstring'
 
     # a simple page that says hello
     @app.route('/')
