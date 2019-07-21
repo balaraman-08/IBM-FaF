093e558ec83499f2e747e23e97946ebec4a8bc8c
Science001 <science.ganesh007@gmail.com>
2019-07-21 13:56:54 +0530

    Handle Errors and return as JSON

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
-
+from flask import Flask, Response, json
+from werkzeug.exceptions import HTTPException
+import traceback
 
 def create_app():
     # create and configure the app
@@ -10,6 +11,36 @@ def create_app():
     @app.route('/')
     def hello():
         return 'Hello, world!'
+
+    # Error Handler
+    @app.errorhandler(Exception)
+    def handle_exception(e):
+        """Return JSON instead of HTML for errors"""
+
+        if(isinstance(e, HTTPException)):
+            # start with the correct headers and status code from the error
+            response = e.get_response()
+            # replace the body with JSON
+            response.data = json.dumps({
+                "code": e.code,
+                "name": e.name,
+                "description": e.description,
+            })
+            response.content_type = "application/json"
+        else:
+            traceback.print_exc()
+            # start with empty response object
+            response = Response(status=500)
+            # add a JSON body
+            response.data = json.dumps({
+                "code": 500,
+                "name": 'Internal Server Error',
+                "description": 'Something Went Wrong',
+            })
+            response.content_type = "application/json"
+
+        return response
+
 
     from . import auth
     app.register_blueprint(auth.auth)
